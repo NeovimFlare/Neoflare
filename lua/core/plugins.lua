23c6d153693b9525970ed0640e57b9b60798a65b
Dynamitebumblemouth <129167212+Dynamitebumblemouth@users.noreply.github.com>
2023-07-07 18:44:40 +0600

    added guihua

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -2,6 +2,7 @@ return {
   { "MunifTanjim/nui.nvim", lazy = true },
   { "nvim-lua/plenary.nvim", lazy = true },
   { "github/copilot.vim", lazy = true },
+  { "ray-x/guihua.lua", lazy = true },
 {
   "folke/flash.nvim", lazy = true,
   event = "VeryLazy",
