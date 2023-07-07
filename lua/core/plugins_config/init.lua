d8b07d747f7ba768ef9081bd9e0233dd77cc4b24
Dynamitebumblemouth <129167212+Dynamitebumblemouth@users.noreply.github.com>
2023-07-08 00:28:20 +0600

    fixed stuff

--- lua/core/plugins_config/init.lua
+++ lua/core/plugins_config/init.lua
@@ -147,3 +147,4 @@ require("lspconfig").pyright.setup {}
 require("lspconfig").clangd.setup {}
 require("lspconfig").tailwindcss.setup {}
 require("lspconfig").rust_analyzer.setup {}
+require("lspconfig").tsserver.setup {}
