a3f503dfa657d667ede7b38b8292f7a773becfb6
jamesgarry1 <141990120+jamesgarry1@users.noreply.github.com>
2023-10-31 13:47:53 +0600

    Update init.lua

--- lua/neoflare/core/plugins_config/init.lua
+++ lua/neoflare/core/plugins_config/init.lua
@@ -208,11 +208,7 @@ require("noice").setup({
 
 vim.opt.list = true
 
-require("indent_blankline").setup {
-    space_char_blankline = " ",
-    show_current_context = true,
-    show_current_context_start = true,
-}
+require("ibl").setup({})
 
 require("mason").setup()
 require("mason-lspconfig").setup({
